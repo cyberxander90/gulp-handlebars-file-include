c0c025f41a10c6fe4a49528a1e0835c3af3f603c
cyberxander90 <cyberxander90@gmail.com>
2017-04-28 22:02:58 -0400

    initial commit.

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+
+// const
+const PLUGIN_NAME = 'gulp-handlebars-file-include';
+
+// gulp-handlebars-file-include plugin
+module.exports = function gulpHandlebarsFileInclude(options) {
+
+    return null;
+};
